1372370a2588adc9da022e5b41b3d5fc3873414f
ZhenyaGleen <evgeniy.goncharov.98@inbox.ru>
2021-05-28 04:35:26 +0300

    Initial commit

---
+++ src/test/java/ex/web/SpringSecurityRegistrationUserApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRegistrationUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
